dd059af8c7e88ba7f532c8f60b7171f0d11fa71b
JAMES (JD) Longmire <Longmire.jd@gmail.com>
2025-08-15 15:19:53 -0500

    Rename LFT to Lean_Proofs throughout codebase
    - Updated lakefile.lean package and lib names
    - Renamed LFT folder to Lean_Proofs
    - Updated all namespace declarations
    - Fixed all import statements
    - Build successful

--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import Mathlib
-import LFT.Basic
+import Lean_Proofs.Basic
 
-def main : IO Unit := IO.println s!"addOne 2 = {LFT.addOne 2}"
+def main : IO Unit := IO.println s!"addOne 2 = {Lean_Proofs.addOne 2}"
